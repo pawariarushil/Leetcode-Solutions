8b825927058a8fbda20b6325474bdc7d01a562fc
Rushil Pawaria <pawariarushil@gmail.com>
2026-08-02 15:34:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.3 MB (14.48%)

--- problems/0125-valid-palindrome/solution.cpp
+++ problems/0125-valid-palindrome/solution.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string final;
+        string s1="";
         for(int i=0;i<s.length();i++){
-            if(!((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z') ||(s[i]>='0' && s[i]<='9'))){
-                continue;
-            }
-            else{
-                final+=tolower(s[i]);
+            if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z')|| (s[i]>='0' && s[i]<='9')){
+                s1+=s[i];
             }
         }
-        string abc=final;
-        reverse(final.begin(),final.end());
-        cout<<abc<<endl<<final;
-        for(int i=0;i<abc.length();i++){
-            if(abc[i]==final[i]){
-                continue;
+        int left=0;
+        int right=s1.length()-1;
+        while(left<right){
+            if(tolower(s1[left])==tolower(s1[right])){
+                left++;
+                right--;
             }
             else{
                 return false;
