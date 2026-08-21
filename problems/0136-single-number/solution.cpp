d712e70af4f16c1fb0af66e924b6a6a1fe7a7a06
Rushil Pawaria <pawariarushil@gmail.com>
2026-08-21 04:57:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.6 MB (58.68%)

--- problems/0136-single-number/solution.cpp
+++ problems/0136-single-number/solution.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums){
+    int singleNumber(vector<int>& nums) {
         int ans=0;
-        for(int x:nums){
-            ans^=x;
+        for(int i=0;i<nums.size();i++){
+            ans^=nums[i];
         }
         return ans;
     }
