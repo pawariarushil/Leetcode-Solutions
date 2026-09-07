9cb4c3fd911102ea9677a962d54a5cc202bc4307
Rushil Pawaria <pawariarushil@gmail.com>
2026-09-07 16:02:03 +0000

    [LeetCode Sync] Runtime - 184 ms (5.34%), Memory - 116.3 MB (5.04%)

--- problems/0217-contains-duplicate/solution.cpp
+++ problems/0217-contains-duplicate/solution.cpp
@@ -1,11 +1,12 @@
-#include<algorithm>
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(mp[nums[i]]>1){
                 return true;
             }
         }
