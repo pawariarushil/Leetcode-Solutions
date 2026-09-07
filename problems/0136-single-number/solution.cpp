561d12fc53b9a301fc2e8436708ecc7d974ebc51
Rushil Pawaria <pawariarushil@gmail.com>
2026-09-07 16:03:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.6 MB (83.90%)

--- problems/0136-single-number/solution.cpp
+++ problems/0136-single-number/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
+        int xorr=0;
         for(int i=0;i<nums.size();i++){
-            ans^=nums[i];
+            xorr^=nums[i];
         }
-        return ans;
+        return xorr;
     }
 };
