0413d0245ba02c7fbe7437ce021fed314b8a0c16
Rushil Pawaria <pawariarushil@gmail.com>
2026-09-07 16:20:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.9 MB (31.64%)

--- problems/0350-intersection-of-two-arrays-ii/solution.cpp
+++ problems/0350-intersection-of-two-arrays-ii/solution.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
+        map<int,int>mp1;
         for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    nums2[j]=-1;
-                    ans.push_back(nums1[i]);
-                    break;
-                }
+            mp1[nums1[i]]++;
+        }
+        for(auto x:nums2){
+            if(mp1[x]>0){
+                ans.push_back(x);
+                mp1[x]--;
             }
         }
         return ans;
