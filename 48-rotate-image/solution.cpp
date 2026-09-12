daef29be19650c2671d72bbbc84c2ad95be5d5f5
Rushil Pawaria <pawariarushil@gmail.com>
2026-09-12 22:43:10 +0530

    Rotate Image - 2026-09-12T17:13:09.703Z

--- 48-rotate-image/solution.cpp
+++ 48-rotate-image/solution.cpp
@@ -1,14 +1,13 @@
-// 0 ms | 10.1 MB
+// 0 ms | 10.2 MB
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i+1;j<matrix.size();j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
-        }   
-        for(int i=0;i<n;i++){
+        }
+        for(int i=0;i<matrix.size();i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
     }
